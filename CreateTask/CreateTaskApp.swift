bf94e701b048567b36a252ffc861fcbd4b3b6a92
AlexEaton0428 <alexander.eaton428@gmail.com>
2022-10-07 10:14:58 -0600

    created create task project

---
+++ CreateTask/CreateTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateTaskApp.swift
+//  CreateTask
+//
+//  Created by Eaton, Alexander on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CreateTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
